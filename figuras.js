efeae4f98bee72d523a5421043b81cc20f063dc2
Victor Carreno <vmanuelcar@hotmail.com>
2022-01-27 18:15:47 -0500

    Actualizacion js - html - css

--- figuras.js
+++ figuras.js
@@ -34,3 +34,68 @@ function perimetroCirculo(radio){
 function areaCirculo(radio){
     return (radio * radio) * PI;
 }
+
+// Funciones HTML
+
+// Cuadrado Función
+
+function calcularCuadrado_P(){
+    const input = document.getElementById("ladoCua");
+    const value = input.value;
+    
+    const perimetro = perimetroCuadrado(value);
+    cuadrado_result.innerHTML = perimetro;
+}
+
+function calcularCuadrado_A(){
+    const input = document.getElementById("ladoCua");
+    const value = input.value;
+
+    const area = areaCuadrado(value);
+    cuadrado_result.innerHTML = area;
+}
+
+// Triángulo Función
+
+function calcularTriangulo_P(){
+    const input_l1 = document.getElementById("ladoT1");
+    const lado1 = input_l1.value;
+
+    const input_l2 = document.getElementById("ladoT2");
+    const lado2 = input_l2.value;
+
+    const input_base = document.getElementById("base");
+    const base = input_base.value;
+
+    const perimetro = perimetroTriangulo(lado1, lado2, base);
+    triangulo_result.innerHTML = perimetro;
+}
+
+function calcularTriangulo_A(){
+    const input_base = document.getElementById("base");
+    const base = input_base.value;
+
+    const input_altura = document.getElementById("altura");
+    const altura = input_altura.value;
+
+    const area = areaTriangulo(base, altura);
+    triangulo_result.innerHTML = area;
+}
+
+// Círculo Función
+
+function calcularCirculo_P(){
+    const input = document.getElementById("radio");
+    const value = input.value;
+
+    const perimetro = perimetroCirculo(value);
+    circulo_result.innerHTML = perimetro;
+}
+
+function calcularCirculo_A(){
+    const input = document.getElementById("radio");
+    const value = input.value;
+
+    const area = areaCirculo(value);
+    circulo_result.innerHTML = area;
+}
